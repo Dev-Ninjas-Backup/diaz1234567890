b9f948e11523805ecd0da9e4a7c88b829658636a
AvijitDasAvi <avijitavi338895@gmail.com>
2025-10-05 05:07:06 +0600

    App Routes file create.

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+class AppRoute {
+  // static String splashScreen = "/splashScreen";
+
+  // static String getSplashScreen() => splashScreen;
+
+  static List<GetPage> routes = [
+    // GetPage(name: splashScreen, page: () => SplashScreen()),
+  ];
+}
