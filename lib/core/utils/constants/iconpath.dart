b8adab5f2f771907d0cb1b385ac7b488163ddece
AvijitDasAvi <avijitavi338895@gmail.com>
2025-10-05 06:40:08 +0600

    Bottom Nav Bar done.

--- lib/core/utils/constants/iconpath.dart
+++ lib/core/utils/constants/iconpath.dart
@@ -1,3 +1,11 @@
 class Iconpath {
   Iconpath();
+  static const String activeHome = 'assets/icons/activeHome.png';
+  static const String inactiveHome = 'assets/icons/inactiveHome.png';
+  static const String activeSearch = 'assets/icons/activeSearch.png';
+  static const String inactiveSearch = 'assets/icons/inactiveSearch.png';
+  static const String activeSellYacht = 'assets/icons/activeSellYacht.png';
+  static const String inactiveSellYacht = 'assets/icons/inactiveSellYacht.png';
+  static const String activeProfile = 'assets/icons/activeProfile.png';
+  static const String inactiveProfile = 'assets/icons/inactiveProfile.png';
 }
