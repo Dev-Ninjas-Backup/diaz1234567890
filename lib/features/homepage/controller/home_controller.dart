5c2f24dbab4a807777dffb6c7ead9b88ff0a1978
Washifur Rahman <69386299+washifur-shihan@users.noreply.github.com>
2025-10-07 04:58:18 +0600

    completed home and search bar search criteria added

--- lib/features/homepage/controller/home_controller.dart
+++ lib/features/homepage/controller/home_controller.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 import '../../../core/utils/constants/imagepath.dart';
---
+++ lib/features/search/controller/yacht_controller.dart
@@ -0,0 +1,34 @@
+import 'package:get/get.dart';
+
+import '../../../core/utils/constants/imagepath.dart';
+import '../model/yacht_model.dart';
+
+class YachtSearchListingController extends GetxController {
+  var similarYachts = <Yacht>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    similarYachts.value = [
+      Yacht(
+        title: "2024 SeaVee 370z",
+        location: "Florida",
+        make: "Mercury",
+        model: "Volvo",
+        year: "2008",
+        price: "\$1,195,000",
+        image: Imagepath.singleBoat,
+      ),
+      Yacht(
+        title: "2007 Hatteras GT",
+        location: "Florida",
+        make: "Mercury",
+        model: "Volvo",
+        year: "2007",
+        price: "\$1,195,000",
+        image: Imagepath.singleBoat,
+      ),
+    ];
+  }
+}
